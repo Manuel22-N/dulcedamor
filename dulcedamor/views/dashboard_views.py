d3e48a7a43effc217cbd74af4fa08b2461614d8e
jmpv08 <jmanosalva2499@gmail.com>
2024-12-18 11:13:22 -0500

    Cambios en la paginacion de categorias y productos, actualizacion en la vista actualizar_producto y modificaciones  en la columna stock de la tabla productos.

--- dulcedamor/views/dashboard_views.py
+++ dulcedamor/views/dashboard_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth import get_user_model  # Obtener el modelo de usuario actual
+from users.models import Categoria,Producto
 
 @login_required(login_url='user_login')  # Redirige al login si no está autenticado
 def dashboard_view(request):
@@ -8,9 +9,14 @@ def dashboard_view(request):
     User = get_user_model()
     # Contar los usuarios registrados
     user_count = User.objects.count()
+
+    category_count = Categoria.objects.count()  # Esto cuenta las categorías
+    product_count = Producto.objects.count()
     
     # Pasar el conteo al contexto de la plantilla
     context = {
         'user_count': user_count,
+        'category_count': category_count,
+        'product_count':product_count
     }
     return render(request, 'dashboard.html', context)
